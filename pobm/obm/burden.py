d11ec7cee17ca3e243c074a4b913c36844e9b431
snir lugassy <snirlugassy@yahoo.com>
2020-11-05 13:24:33 +0200

    docs

--- pobm/obm/burden.py
+++ pobm/obm/burden.py
@@ -10,12 +10,11 @@ class HypoxicBurdenMeasures:
     """
     Class that calculates Hypoxic Burden Features from spo2 time series.
     The method compute runs all the biomarker of this category.
-
     """
 
     def __init__(self, begin: np.array, end: np.array, CT_Threshold: float = 90, CA_Baseline: float = None):
         """
-        
+
         :param begin: Numpy array of indices of beginning of each desaturation event.
         :type begin: Numpy array
         :param end: Numpy array of indices of end of each desaturation event. begin and end should have the same length.
@@ -45,11 +44,11 @@ class HypoxicBurdenMeasures:
         :param signal: 1-d array, of shape (N,) where N is the length of the signal
         :return: HypoxicBurdenMeasuresResults class containing the following features:
 
-            * CA: Integral SpO2 below the xx SpO2 level normalized by the total recording time
-            * CT: Percentage of the time spent below the xx% oxygen saturation level
-            * POD: Percentage of oxygen desaturation events
-            * AODmax: The area under the oxygen desaturation event curve, using the maximum SpO2 value as baseline and normalized by the total recording time
-            * AOD100: Cumulative area of desaturations under the 100% SpO2 level as baseline and normalized by the total recording time
+            - CA: Integral SpO2 below the xx SpO2 level normalized by the total recording time
+            - CT: Percentage of the time spent below the xx% oxygen saturation level
+            - POD: Percentage of oxygen desaturation events
+            - AODmax: The area under the oxygen desaturation event curve, using the maximum SpO2 value as baseline and normalized by the total recording time
+            - AOD100: Cumulative area of desaturations under the 100% SpO2 level as baseline and normalized by the total recording time
 
 
         Example:
